f036d0abe80d9967d32eba52ad085691a9058878
Andrzej Kawula <andrzej.kawula@gmail.com>
2024-05-25 20:20:00 +0200

    feat: templ ctx has active url
    Extended ctx has activeUrl variable so global view that other templates
    extend can set menu items as active :)

--- handler/util.go
+++ handler/util.go
@@ -1,10 +1,13 @@
 package handler
 
 import (
+	"context"
+
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
 
 func render(c echo.Context, comp templ.Component) error {
-	return comp.Render(c.Request().Context(), c.Response())
+	ctx := context.WithValue(c.Request().Context(), "activeUrl", c.Request().URL.String())
+	return comp.Render(ctx, c.Response())
 }
